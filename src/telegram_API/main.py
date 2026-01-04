da68f547b65c61a908ba5141d10db72fd8448fac
meti2626 <metseboka@gmail.com>
2026-01-04 22:12:59 +0300

    1/4/2026-first commit

--- src/telegram_API/main.py
+++ src/telegram_API/main.py
@@ -1,7 +1,7 @@
 import asyncio
 import os
 from dotenv import load_dotenv
-from telethon import TelegramClient
+from telethon import TelegramClient, events
 
 os.environ.clear()
 load_dotenv(override=True)
@@ -9,10 +9,15 @@ load_dotenv(override=True)
 TELEGRAM_API_ID = os.environ.get('TELEGRAM_API_ID')
 TELEGRAM_API_HASH =os.environ.get('TELEGRAM_API_HASH')
 async def main():
-  
+     
      client = TelegramClient('session_name', TELEGRAM_API_ID, TELEGRAM_API_HASH)
+     @client.on(events.NewMessage)
+     async def handle_message(event):
+           print(event.message.text)
+           await event.reply(event.message.text)
      await client.start()
      await client.send_message('me', 'Hello, myself!')
-     return
+     await client.run_until_disconnected()
+     return handle_message
 
 asyncio.run(main())
