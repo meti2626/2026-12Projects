ee04560fcd88a029ce1ee77155dcf540fe8b029e
meti2626 <metseboka@gmail.com>
2026-01-23 21:02:37 +0300

    1/23/2026

---
+++ python/ConditionalStatement.py
@@ -0,0 +1,38 @@
+# cars = ['audi', 'bmw', 'subaru', 'toyota']
+
+
+# for car in cars:
+#   if car == 'btw':
+#     print(car.upper())
+#   else:
+#     print(car.title())
+
+
+# car = 'btm'
+# print(car == 'BTW')
+
+
+# age=22
+# age_2 = 18
+
+# if age >=21 or age_2 >=21:
+#   print("you are old enough to drink")
+
+
+# banned_users = ['andrew', 'carolina', 'david']
+# user = 'andrew'
+
+# if user not in banned_users:
+#   print(f"{user.title()}, you can post a response if you wish.")
+# else:
+#     print(f"{user.title()}, you are banned from posting.")
+
+
+# requested_topping = ['muhroom', 'onion', 'pineapple']
+# print('muhroom' in requested_topping)
+
+
+
+
+
+
