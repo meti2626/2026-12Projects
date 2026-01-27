759355ff8bfb3491b8bf69eddb405a88ed9a56e4
meti2626 <metseboka@gmail.com>
2026-01-27 11:40:11 +0300

    1/27/2026

--- python/function.py
+++ python/function.py
@@ -91,22 +91,38 @@
 #     print(completed_model)
 
 
-def print_models(unprinted_designs, completed_models):
+# def print_models(unprinted_designs, completed_models):
    
-   while unprinted_designs:
-    current_design = unprinted_designs.pop()
-    print(f"Printing model: {current_design}")
-    completed_models.append(current_design)
+#    while unprinted_designs:
+#     current_design = unprinted_designs.pop()
+#     print(f"Printing model: {current_design}")
+#     completed_models.append(current_design)
 
-def show_completed_models(completed_models):
+# def show_completed_models(completed_models):
 
-  print("\nThe following models have been printed:")
-  for completed_model in completed_models:
-     print(completed_model)
+#   print("\nThe following models have been printed:")
+#   for completed_model in completed_models:
+#      print(completed_model)
 
-unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
-completed_models = [] 
+# unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+# completed_models = [] 
 
 
-print_models(unprinted_designs, completed_models)
-show_completed_models(completed_models)
+# print_models(unprinted_designs, completed_models)
+# show_completed_models(completed_models)
+
+
+
+#---PASSING AN ARBITRARY NUMBER OF ARGUMENTS----#
+
+
+
+
+def make_pizza(*toppings,size):
+   print(f"\nMaking a {size}-inch pizza with the following toppings:") 
+   for topping in toppings:
+    print(f"-{topping}")
+
+
+make_pizza('pepperoni','margatita', 12 )
+make_pizza(16, 'mushrooms','green peppers','extra cheese')
