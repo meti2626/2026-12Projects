34a6812000b3733c348f958a1940bc56b6b3edd2
meti2626 <metseboka@gmail.com>
2026-02-06 23:02:01 +0300

    2/6/2026-commit#1

--- python/file_reader.py
+++ python/file_reader.py
@@ -1,9 +1,3 @@
-with open('name.txt') as file_object:  
-   # to do work with file you have toopen it first hence - comes open() - need the file you want to open as an argument
-   contents = file_object.read()
 
-print(contents.rstrip())  
-
-# rstrip() method removes any whitespace characters from the right side of a string
 
 
